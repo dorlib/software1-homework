c4a9512c2a0052b626d9da3fca1a02bfea7a5dd8
Dor Liberman <90474428+dorlib@users.noreply.github.com>
2023-02-14 22:51:54 +0200

    Delete hw9-dorlib directory

--- hw9-dorlib/src/il/ac/tau/cs/sw1/ex9/riddles/first/A1.java
+++
@@ -1,8 +0,0 @@
-package il.ac.tau.cs.sw1.ex9.riddles.first;
-
-public class A1 {
-	protected boolean foo() {
-
-		return true;
-	}
-}
--- hw9-dorlib/src/il/ac/tau/cs/sw1/ex9/starfleet/CrewMember.java
+++
@@ -1,7 +0,0 @@
-package il.ac.tau.cs.sw1.ex9.starfleet;
-
-public interface CrewMember {
-    String getName();
-    int getAge();
-    int getYearsInService();
-}
